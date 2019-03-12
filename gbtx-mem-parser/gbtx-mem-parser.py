7de1e7cca655b5fcbf7c8222939b012f83941d1d
LegyPlegy <40072973+LegyPlegy@users.noreply.github.com>
2019-03-12 11:38:29 -0400

    Update gbtx-mem-parser.py
    code is now usable

--- gbtx-mem-parser/gbtx-mem-parser.py
+++ gbtx-mem-parser/gbtx-mem-parser.py
@@ -4,19 +4,55 @@ Created on Mon Mar 11 12:41:00 2019
 @author: JorgeR
 """
 
-def GetData(filepath):  # method will parse a given txt file
+def GetData(filepath):  # method will parse a given txt file path
     with open(filepath) as file:
          data = file.readlines()  # place each line into a list named 'data'
-    
     return data
+    
+def ExtractBits(x, y, memlist):  # returns the xth and yth digits
+    bitlist = []
+    
+    for line in memlist:
+        bitlist.append(line[x-1:y])
+        
+    return bitlist
 
+def IdentifyBits(bitlist):  # identify unique bits
+    uniques = []
+    
+    for bit in bitlist:
+        if bit not in uniques:  # TODO: count each appearance of unique simult.
+            uniques.append(bit)
+            
+    return uniques
 
-def PrintLines(memlist):  # debug method
+def ConvertBits(hexlist, string):  # general conversion method
+    outputlist = []
+    
+    if string == "int":
+        for num in hexlist:
+            outputlist.append(int(num, 16))
+            
+    if string == "bin": 
+        for num in hexlist:
+            outputlist.append(bin(int(num, 16))[2:])  # remove "0b" with [2:]
+            
+    return outputlist
+
+##########Debug Methods##############
+
+def PrintLines(memlist):  # debug method, spits out entire raw list
     for line in memlist:
         print(line)
+
+def main(filepath, x, y):
+    data = GetData(filepath)
+    bitlist = ExtractBits(6, 7, data)
+    uniques = IdentifyBits(bitlist)
+    print(uniques)
+    print(ConvertBits(uniques, "bin"))
     
+main("mem_mon_table-const.txt", 6, 7)  # bits are on indices 6 and 7
 
-###Debug
-unparsed_memory = GetData("mem_mon_table-const.txt")
-PrintLines(unparsed_memory)
-
+####### METHODS TO ADD #######
+### identify "legal" permutations
