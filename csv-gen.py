4858a8a9d8bbed410bcde08874512c6f84b30b3c
Yipeng Sun <syp@umd.edu>
2019-03-15 01:26:04 -0400

    More concise coding.

--- csv-gen.py
+++ csv-gen.py
@@ -1,84 +1,42 @@
 #!/usr/bin/env python
-"""
-Created on Wed Jan 23 08:42:00 2019
+#
+# Authors: Ramond Su, Yipeng Sun
+# License: BSD 2-clause
+# Last Change: Fri Mar 15, 2019 at 01:25 AM -0400
 
-@author: raymo
-"""
-import csv
+from pathlib import Path
 
-## Kludge
-
-serial_numbers =  [[["00100000"]],[["00100001"]],[["00100010"]],[["00100011"]],
-                  [["00100100"]],[["00100101"]],[["00100110"]],[["00100111"]],
-                  [["00101000"]],[["00101001"]],[["00101010"]],[["00101011"]],
-                  [["00101100"]],[["00101101"]],[["00101110"]],[["00101111"]],
-                  [["00110000"]],[["00110001"]],[["00110010"]],[["00110011"]]]
-
-## CSV Preamble, lines 1 and 2 correspond to the two items.
-    
-csvPreamble = [["260"],["10101010"]]
-
-## CSV "Junk Data" initializer
-  
-csvData = []
-Data_Line_1 = ["01010101"]
-
-## CSV Closer
-
-csvCloser = [["00000000"],["01010101"]] 
-
-## Loop to create csvData list.
-
-"""
-Used in regular CSV.
-x = 0
-while x < 256:
-    csvData.append(Data_Line_1)
-    x = x + 1
-
-"""
-
-count = list(range(0,256))
-x = list(range(0,256))
-
-for n in x:
-    x[n] = format(x[n], '08b')
+output_dir = Path('gen')
 
 
-## Look to create 20 CSV files, with preamble.
-for y in range(0, 20):
-    Name = 'nTx_seq_' + str(y) + '.csv'
-    ## csvPreamble_Serial = str(BinaryRange[y])
-    with open(Name, 'w') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerows(csvPreamble)
-        writer.writerows(serial_numbers[y])
-        for z in count:
-            writer.writerow([x[z]])
-        writer.writerows(csvCloser)
-        csvFile.close()
+##########
+# Output #
+##########
 
-"""
-Reference on how CSV reads data
-csvData = [[1], [22], [21], [24]]
+# Each csv file requires a different serial number. Here we generate all 20 at
+# once.
+serial_numbers = ['{0:08b}'.format(n)
+                  for n in range(int('00100000', 2), int('00100000', 2)+20)]
 
-Opening preamble:
-260
-10101010
-00100000
+csv_preamble = ["260", "10101010"]
+csv_epilogue = ["00000000", "01010101"]
 
 
-## Binary Range Generator, for the third line of the preamble.
+def write_to_comet_csv(filename, serial, body, mode='w', eol='\n'):
+    with open(filename, mode) as f:
+        for row in csv_preamble+[serial]+body+csv_epilogue:
+            f.write(row + eol)
 
-FileRange = list(range(0,20))
-BinaryRange = []
-for x in FileRange:
-    Number_Binary = "{0:b}".format(FileRange[x])
-    BinaryRange.append(Number_Binary)
-    
 
-while x < 255:
-    my_randoms = random.sample(range(100), 8)
-    csvData.append(my_randoms)
-    x = x + 1
-"""
+############
+# Patterns #
+############
+
+def fixed_pattern(mode='01010101', length=256):
+    return [mode for i in range(0, length)]
+
+
+def cyclic_pattern(head='00000000', length=256, start_idx=0):
+    ref_cyclic_pattern = ['{0:08b}'.format(n)
+                          for n in range(int(head, 2), int(head, 2)+length)]
+    return ref_cyclic_pattern[start_idx:] + ref_cyclic_pattern[:start_idx]
