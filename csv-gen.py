37b716815c219186dea977347754a7c628abf2aa
Yipeng Sun <syp@umd.edu>
2019-03-15 01:43:05 -0400

    Write number in binary representation directly.

--- csv-gen.py
+++ csv-gen.py
@@ -2,7 +2,7 @@
 #
 # Authors: Ramond Su, Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Mar 15, 2019 at 01:32 AM -0400
+# Last Change: Fri Mar 15, 2019 at 01:44 AM -0400
 
 from pathlib import Path
 
@@ -16,7 +16,7 @@ output_dir = Path('gen')
 # Each csv file requires a different serial number. Here we generate all 20 at
 # once.
 serial_numbers = ['{0:08b}'.format(n)
-                  for n in range(int('00100000', 2), int('00100000', 2)+20)]
+                  for n in range(0b00100000, 0b00100000+20)]
 
 csv_preamble = ["260", "10101010"]
 csv_epilogue = ["00000000", "01010101"]
@@ -32,13 +32,13 @@ def write_to_comet_csv(filename, serial, body, mode='w', eol='\n'):
 # Patterns #
 ############
 
-def fixed_pattern(mode='01010101', length=256):
-    return [mode for i in range(0, length)]
+def fixed_pattern(mode=0b01010101, length=256):
+    return ['{0:08b}'.format(mode) for i in range(0, length)]
 
 
-def cyclic_pattern(head='00000000', length=256, start_idx=0):
+def cyclic_pattern(head=0b00000000, length=256, start_idx=0):
     ref_cyclic_pattern = ['{0:08b}'.format(n)
-                          for n in range(int(head, 2), int(head, 2)+length)]
+                          for n in range(head, head+length)]
     return ref_cyclic_pattern[start_idx:] + ref_cyclic_pattern[:start_idx]
 
 
