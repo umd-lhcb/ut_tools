63cf78427a715ed8e4341f500653b6ed113b9100
Yipeng Sun <syp@umd.edu>
2019-04-11 14:39:10 -0400

    Rename channels; simpler way to parse GBTx packets.

--- GbtxMemParser.py
+++ GbtxMemParser.py
@@ -19,20 +19,18 @@ class GbtxMemParser(object):
         Returns:
             parsed_data (list): A list of dictionary. Each dictionary has the
                 following form:
-                    {'elink0-0': <1-byte int>,
-                     'elink0-1': <1-byte int>,
-                     'elink1-0': <1-byte int>,
-                     'elink1-1': <1-byte int>,
-                     'elink2-0': <1-byte int>,
-                     'elink2-1': <1-byte int>,
-                     'elink3-0': <1-byte int>,
-                     'elink3-1': <1-byte int>,
-                     'elink4-0': <1-byte int>,
-                     'elink4-1': <1-byte int>,
-                     'elink5-0': <1-byte int>,
-                     'elink5-1': <1-byte int>,
-                     'elink6-0': <1-byte int>,
-                     'elink6-1': <1-byte int>}
+                    {'elink0': <1-byte int>,
+                     'elink1': <1-byte int>,
+                     'elink2': <1-byte int>,
+                     'elink3': <1-byte int>,
+                     'elink4': <1-byte int>,
+                     'elink5': <1-byte int>,
+                     'elink6': <1-byte int>,
+                     'elink7': <1-byte int>,
+                     'elink8': <1-byte int>,
+                     'elink9': <1-byte int>,
+                     'elink10': <1-byte int>,
+                     'elink11': <1-byte int>}
                 Note that the header is removed from the final result.
         '''
         with open(self.filename) as f:  # open txt file
@@ -40,8 +38,8 @@ class GbtxMemParser(object):
 
         return self.dissect_str_to_dict(raw_data)
 
-    @staticmethod
-    def dissect_str_to_dict(raw_data):
+    @classmethod
+    def dissect_str_to_dict(cls, raw_data):
         '''Take a list of string, dissecting them to list of dictionary.
 
         Parameters:
@@ -52,83 +50,48 @@ class GbtxMemParser(object):
             parsed_data (list): Same form as defined in 'parse' method.
                 This is the normal output of the parser.
         '''
+        elink_idx_map = cls.elink_channel_idx_mapping()
+        parsed_data = []
 
-        default = (204)    # placeholder- (204) = (CC)
-        dict_list = []
-        sample_dict = {       # sample egroup_dict, dummy values
-            'elink6-0': default,
-            'elink6-1': default,
-            'elink5-0': default,
-            'elink5-1': default,
-            'elink4-0': default,
-            'elink4-1': default,
-            'elink3-0': default,
-            'elink3-1': default,
-            'elink2-0': default,  # elink identifiers (strings)
-            'elink2-1': default,  # key[5] = 2, key[7] = 1
-            'elink1-0': default,
-            'elink1-1': default,
-            'elink0-0': default,
-            'elink0-1': default,
+        for raw_packet in raw_data:
+            parsed_packet = {}
+            for elink, idx in elink_idx_map.items():
+                idx = len(raw_packet) - idx - 1
+                parsed_packet[elink] = int(raw_packet[idx-2:idx], 16)
+            parsed_data.append(parsed_packet)
+
+        return parsed_data
+
+    @staticmethod
+    def elink_channel_idx_mapping():
+        '''Define the mapping between elink channel name to its byte index in
+        the GBTx frame.
+
+        Counting from right: idx 0 -> rightmost.
+        '''
+        return {
+            'elink0': 2,
+            'elink1': 0,
+            'elink2': 6,
+            'elink3': 4,
+            'elink4': 10,
+            'elink5': 8,
+            'elink6': 14,
+            'elink7': 12,
+            'elink8': 18,
+            'elink9': 16,
+            'elink10': 22,
+            'elink11': 20
         }
-
-        # Elink ordering: 6 (0,1) ,5 (0,1), 4 (0,1), 3 (0,1), 2 (0,1), 1 (0,1),
-        #                 0 (0,1)
-
-        # begin by looping through every line in the data file
-        for string_of_hexes in raw_data:
-            unsorted_elinks = []  # make sure to refresh our list every loop
-            elink_dict = sample_dict.copy()  # refresh dict every loop
-
-            # extract the data from the line we grabbed, sort into elinks
-            for index, digit in enumerate(string_of_hexes):
-
-                # use every even index, skipping the header (indices 0-3),
-                # and extract that index and the next one which we convert to decimal.
-                # every line has a pesky "\n" at the end so we remove it
-                if digit != "\n" and index > 3 and index % 2 == 0:
-                    unsorted_elinks.append(int(string_of_hexes[index:index+2], 16))
-
-            # match each key in the sample dictionary to its correct index
-            for key in elink_dict:
-                x, y = int(key[5]), int(key[7])  # see sample dict
-
-                # use magic formula to match values to keys... see comment below
-                for index, value in enumerate(unsorted_elinks):
-                    if index == (2*(6-x)+y):  # magic formula
-                        elink_dict[key] = value  # found match
-
-            dict_list.append(elink_dict)  # save our finalized dictionary into list
-
-        return dict_list  # after we loop through every line, return our list
-
-        '''
-        0-0 -> index 12
-        0-1 -> index 13
-        1-0 -> index 10
-        1-1 -> index 11
-        ...
-        5-0 -> index 2
-        5-1 -> index 3
-        6-0 -> index 1
-        6-1 -> index 0
-
-        x-y -> index z
-        z = (2(6-x) + y)
-
-        see github issue 8 (https://github.com/umd-lhcb/comet_tools/issues/8)
-        for explanation on magic formula
-
-        '''
 
     @staticmethod
     def output_to_csv(parsed_data, filename="gen/parsed_elinks.csv"):
         '''Write parsed memory data to a CSV file.
 
         Parameters:
+            parsed_data (list): Same form as defined in 'parse' method.
             filename (Path or str): Output CSV filename, full path or relative
                 path.
-            parsed_data (list): Same form as defined in 'parse' method.
 
         Returns: None
         '''
