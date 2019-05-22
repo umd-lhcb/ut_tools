12f5c2a0a082cdc9b2a7c1df47362e6ea9ee5615
Yipeng Sun <syp@umd.edu>
2019-05-22 16:49:28 -0400

    Use 'tabulate' package to simplify output.

--- GbtxMemLongestPattern.py
+++ GbtxMemLongestPattern.py
@@ -11,6 +11,7 @@ import re
 
 from collections import defaultdict
 from sty import fg, ef, rs
+from tabulate import tabulate
 
 from ut_tools.GbtxMemParser import GbtxMemParser
 from ut_tools.GbtxMemAnalyzer import ref_cyclic_pattern
@@ -21,13 +22,6 @@ from ut_tools.GbtxMemAnalyzer import check_time_evolution
 ###########
 # Helpers #
 ###########
-
-# Returns e-link name given a global index. It allows us to modularly change e-
-# link order.
-# Currently it uses the order directly in files.
-def elink_names():
-    return ['elink' + str(i) for i in range(0, 12)]
-
 
 def generate_path_to_all_mem_files(
         path_suffix,
@@ -80,11 +74,11 @@ def regularize_comet_dcb_mapping(path='input/CometDcbShortMapping.csv'):
 
 slice_size = 2
 data_slice_size = slice_size + 1
-threshold = 5  # Consider an elink alive if it has at least 5 counting bytes
+threshold = 5   # Consider an elink alive if it has at least 5 counting bytes
 total_num = 72  # Total number of elinks
 
+elink_names = ['elink' + str(i) for i in range(0, 12)]
 comet_prefix = sys.argv[1]
-preferred_elink_names = elink_names()
 
 
 ##############
@@ -92,72 +86,65 @@ preferred_elink_names = elink_names()
 ##############
 
 comet_mem_files = generate_path_to_all_mem_files(comet_prefix)
-all_parsed_data = {comet: {gbtx: parse_mem_file(f)
-                           for gbtx, f in inner.items()}
-                   for comet, inner in comet_mem_files.items()}
+parsed_data = {comet: {gbtx: parse_mem_file(f)
+                       for gbtx, f in inner.items()}
+               for comet, inner in comet_mem_files.items()}
 
-ref_patterns = ref_cyclic_pattern(preferred_elink_names, [1]*12)
+ref_patterns = ref_cyclic_pattern(elink_names, [1]*12)
 sliced_patterns = slice_ref_patterns(ref_patterns, slice_size=slice_size)
-all_test_results = {comet: {
+test_result = {comet: {
     gbtx: check_time_evolution(sliced_patterns, data,
                                data_slice_size=data_slice_size,
                                slice_size=slice_size)
     for gbtx, data in inner.items()}
-    for comet, inner in all_parsed_data.items()}
-
-final_result = {gbtx: {elink: {
-    'from': 'none', 'direction': 'none', 'length': 0}
-    for elink in preferred_elink_names} for gbtx in range(1, 7)}
+    for comet, inner in parsed_data.items()}
 
 
 ##########
 # Output #
 ##########
 
+final_result = {gbtx: {elink: {
+    'from': 'none', 'direction': 'none', 'length': 0}
+    for elink in elink_names} for gbtx in range(1, 7)}
+
+# Determine the COMET variant of each elink on each GBTx
 for gbtx in range(1, 7):
     for comet in ['a', 'b']:
-        for elink in preferred_elink_names:
+        row = ['GBTx-{}'.format(gbtx)]
+        for elink in elink_names:
             counting_length = \
-                all_test_results[comet][gbtx][elink]['counting_length']
+                test_result[comet][gbtx][elink]['counting_length']
 
             if counting_length > threshold:
                 final_result[gbtx][elink]['from'] = comet
 
                 final_result[gbtx][elink]['direction'] = \
-                    all_test_results[comet][gbtx][elink]['counting_direction']
+                    test_result[comet][gbtx][elink]['counting_direction']
 
                 final_result[gbtx][elink]['length'] = counting_length
 
 elink_counting_directions = [final_result[gbtx][elink]['direction']
                              for gbtx in range(1, 7)
-                             for elink in preferred_elink_names]
+                             for elink in elink_names]
 num_counting_up = \
     len(list(filter(lambda x: x == 'up', elink_counting_directions)))
 num_counting_down = \
     len(list(filter(lambda x: x == 'down', elink_counting_directions)))
 num_dead = total_num - num_counting_up - num_counting_down
 
-comet_dcb_map = regularize_comet_dcb_mapping()
-comet_pins_to_check = []
-
 print('\nFound {0} e-links counting up, {1} counting down, and {2} dead\n'.format(
     num_counting_up, num_counting_down, num_dead
 ))
 
-# Printing header with e-link names
-width = 13
 
-length_of_gbtx = len('GBTx-1')
-print(' '*length_of_gbtx, end='')
-
-for elink in preferred_elink_names:
-    print('{0:>{1}}'.format(elink, width), end='')
-print()
-
-print('-'*(length_of_gbtx + width*len(preferred_elink_names)))
+# Generate output tables
+comet_dcb_map = regularize_comet_dcb_mapping()
+output_gbtx = []
+output_problematic_elinks_info = []
 
 for gbtx, elinks in final_result.items():
-    print('GBTx-{}'.format(gbtx), end=' ')
+    row = ['GBTx-{}'.format(gbtx)]
     for elink_name, elink_info in elinks.items():
         elink_id = str(gbtx) + '-' + elink_name
         comet_pins = ','.join(comet_dcb_map[elink_id])
@@ -165,30 +152,24 @@ for gbtx, elinks in final_result.items():
         comet = elink_info['from']
         if comet == 'a':
             formatted_comet = fg.blue + ef.bold + \
-                '{:>4}'.format(comet.upper()) + rs.bold_dim + fg.rs
+                comet.upper() + rs.bold_dim + fg.rs
         elif comet == 'b':
             formatted_comet = fg.yellow + ef.bold + \
-                '{:>4}'.format(comet.upper()) + rs.bold_dim + fg.rs
+                comet.upper() + rs.bold_dim + fg.rs
         else:
             formatted_comet = fg.red + ef.bold + \
-                '{:>4}'.format(comet.upper()) + rs.bold_dim + fg.rs
-            comet_pins_to_check.append((elink_id, comet_pins))
-        print(formatted_comet, end=',')
+                comet.upper() + rs.bold_dim + fg.rs
+            output_problematic_elinks_info.append([elink_id, comet_pins])
 
         direction = elink_info['direction']
-        print('{:>4}'.format(direction), end=',')
-
         length = str(elink_info['length'])
-        print('{0:>{1}} '.format(length, width-4-4-1-1-1), end='')
-    print()
+        row.append(','.join([formatted_comet, direction, length]))
+    output_gbtx.append(row)
 
+# Print the GBTx output table
+print(tabulate(output_gbtx,
+               headers=['']+elink_names,
+               colalign=['left']+['right']*len(elink_names)))
 print('\nAdditional info for the dead elinks:\n')
-
-print('{:>9}'.format('elink'), end='  ')
-print('{:>15}'.format('COMET FPGA pins'))
-print('-'*(9 + 15 + 2))
-
-for row in comet_pins_to_check:
-    elink_id, comet_pins = row
-    print('{:>9}'.format(elink_id), end='  ')
-    print('{:>15}'.format(comet_pins))
+print(tabulate(output_problematic_elinks_info,
+      headers=['elink', 'COMET FPGA pins']))
